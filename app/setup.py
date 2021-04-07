cf17b8cb375ffff8b0d0c19382a29512638bfc4b
Mykyta <samovarov@ucu.edu.ua>
2021-04-07 13:54:45 +0300

    edited setup

--- app/setup.py
+++ app/setup.py
@@ -1,14 +1,17 @@
-from alembic.config import Config
-from os import getenv
+import os
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
 
+from alembic import context
 
-def get_path():
-	return "postgresql://{}:{}@{}/{}".format(getenv('PSQL_LOG'),\
-						getenv('PSQL_PASS'),\
-						getenv('PSQL_URL'),\
-						getenv('PSQL_DB_NAME'))
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
 
-
-alembic_cfg = Config()
-path = get_path()
-alembic_cfg.set_main_option("sqlalchemy.url", path)
+# here we allow ourselves to pass interpolation vars to alembic.ini
+# fron the host env
+section = config.config_ini_section
+config.set_section_option(section, "PSQL_LOG", os.environ.get('PSQL_LOG'))
+config.set_section_option(section, "PSQL_PASS", os.environ.get('PSQL_LOG'))
+config.set_section_option(section, "PSQL_URL", os.environ.get('PSQL_URL'))
+config.set_section_option(section, "PSQL_DB_NAME", os.environ.get('PSQL_DB_NAME'))
