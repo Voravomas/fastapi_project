be0f90fe1ab52f3535a9e4d3b9ad251a5dd03a8b
Mykyta <samovarov@ucu.edu.ua>
2021-04-07 13:05:15 +0300

    Added setup.py file for config alembic

---
+++ app/setup.py
@@ -0,0 +1,13 @@
+from alembic.config import Config
+
+
+def get_path():
+	return "postgresql://{}:{}@{}/{}".format(getenv('PSQL_LOG'),\
+						getenv('PSQL_PASS'),\
+						getenv('PSQL_URL'),\
+						getenv('PSQL_DB_NAME'))
+
+
+alembic_cfg = Config()
+path = get_path()
+alembic_cfg.set_main_option("sqlalchemy.url", path)
