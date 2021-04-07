49caeadfe31b02c2b956786994a6ed1faea8178e
Mykyta <samovarov@ucu.edu.ua>
2021-04-07 15:53:16 +0300

    Edited startup.sh

--- app/startup.sh
+++ app/startup.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 # waiting for db to start
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h $PSQL_URL -U $POSTGRES_USER -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
+# echo "Waiting for db to init"
+# sleep 5
 
 # pulling any new changes
 git pull
